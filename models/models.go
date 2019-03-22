7b996f4efff101596e16167201209d35255e19d1
Stanislav Kostenko <stanislav_kostenko@epam.com>
2019-03-22 12:31:12 +0200

    Finished models. Add gitignore. Add dep

--- models/models.go
+++ models/models.go
@@ -1,30 +1,46 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 //CurrencyState struct
 type CurrencyState struct {
-	sourceID      string
-	updateDate    time.Time
-	organizations Organization
+	SourceID      string
+	UpdateDate    time.Time
+	Organizations Organization
 }
 
 //Organization for currency
 type Organization struct {
+	OrgType    OrganizationType
+	Title      string
+	CityID     string
+	Address    string
+	Link       string
+	Currencies []Currency
 }
 
 //OrganizationType for organization
 type OrganizationType struct {
+	TypeID   int
+	TypeName string
 }
 
 //Currency entity
 type Currency struct {
+	LongName string
+	Name     string
 }
 
 //Region entity
 type Region struct {
+	RegionID   string
+	RegionName string
 }
 
 //City entity
 type City struct {
+	CityID   string
+	CityName string
 }
