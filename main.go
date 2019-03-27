fd38f51d07ab2ef82028d96d8b148b9d65aad7cf
Stanislav Kostenko <stanislav_kostenko@epam.com>
2019-03-27 18:17:01 +0200

    Add telegram bot

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"ua-currency-notifier/pkg/currency"
+	"ua-currency-notifier/pkg/telegram"
 )
 
 func main() {
@@ -13,5 +14,15 @@ func main() {
 	}
 	for _, v := range currencies.Organizations {
 		fmt.Println(v)
+	}
+
+	bot, updates, err := telegram.BotInit("")
+	if err != nil {
+		log.Printf("[ERROR] %v", err)
+	}
+
+	err = telegram.Run(bot, updates)
+	if err != nil {
+		log.Printf("[ERROR] %v", err)
 	}
 }
